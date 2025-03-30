56fbfd32b921f4cd28bce60b3570cf98003aa9fd
karan-2006 <karansunil2006@gmail.com>
2025-03-30 15:19:02 +0530

    Modified index style and scrript. updated images.deleted unwanted ootstrap styles folder

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-function big(img){
-
+function big(img) {
     document.querySelector('.big').src = img;
-}
-
-function path(color) {
-    const path = document.querySelector('.path');
-    path.style.background = color;
-}
-function bg(background){
-    const bg = document.querySelector(".background");
-    bg.style.background=background;
-    
-}
-function changeText(newText, color) {
-    const displayText = document.querySelector('#displayText')
+  }
+  
+  function path(color) {
+    document.querySelector('.path').style.background = color;
+  }
+  
+  function bg(background) {
+    document.querySelector(".background").style.background = background;
+  }
+  
+  function changeText(newText, color) {
+    const displayText = document.querySelector('#displayText');
     displayText.textContent = newText;
     displayText.style.color = color;
-}
-function modeText(color) {
-    const showText= document.querySelector('h1');
-    showText.style.color = color; 
-}
+  }
+  
+  function modeText(color) {
+    const showText = document.querySelector('h1');
+    showText.style.color = color;
+  }
+  
